4a248014deb1a0b9c3ad76c1e667d3ba6fff49b8
Mahidhar <reddymahidhar9@gmail.com>
2021-03-24 23:23:47 -0400

    Heroku

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 
 const app = express();
-
+dotenv.config();
  
 
 app.use(express.json({ limit : "30mb", extended : true }));
@@ -16,10 +15,14 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://admin:Admin@123@cluster0.uahf8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+    res.send('LASTCHANCE API');
+});
+
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
  
