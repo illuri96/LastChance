ef89a1be1ad7421e6e48a83d4966b1d861a85045
Mahidhar Illuri <reddymahidhar9@gmail.com>
2021-04-20 19:31:10 -0400

    home-page completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Link } from 'react-router-dom';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
+import Header from './components/Navbar/Navbar'
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import { getPosts } from './actions/posts';
@@ -12,28 +15,43 @@ const App = () => {
   const dispatch = useDispatch();
   const classes = useStyles();
 
+  const sections = [
+    { title: 'Technology', url: '#' },
+    { title: 'Culture', url: '#' },
+    { title: 'Business', url: '#' },
+    { title: 'Politics', url: '#' },
+    { title: 'Opinion', url: '#' },
+    { title: 'Science', url: '#' },
+    { title: 'Health', url: '#' },
+    { title: 'Cinema', url: '#' },
+    { title: 'Travel', url: '#' },
+  ];
+  
+
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
 
   return (
-    <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">LastChance</Typography>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+          <Header title="Blog" sections={sections} />
+          <Container maxWidth="lg">
+            <Grow in>
+              <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={4} >
+                <Grid item xs={12} sm={7}>
+                  <Posts setCurrentId={setCurrentId} />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Form currentId={currentId} setCurrentId={setCurrentId} />
+                </Grid>
+              </Grid>
+            </Grow>
         </Container>
-      </Grow>
     </Container>
+  </React.Fragment>
+
   );
 };
 
